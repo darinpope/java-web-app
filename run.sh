7176e67a43471ac7f15eb31ae90a60f8d125394e
JorgeCantu247 <jorge.cantu@atomic32.com>
2023-06-07 11:55:39 -0600

    m

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ java -version
 NOW=$(date +"%Y-%m-%d %H:%M:%S %Z")
 NETTY_TMP=-Dio.netty.native.workdir=/tmp
 SECURITY_OPT=-Djava.security.egd=file:/dev/./urandom
-PORT=$(echo "$1" | awk '{print $0+0}')  # Convertir a entero
+PORT=$(($1))  # Convertir a entero
 
 echo
 echo "TZ            <$TZ>"
@@ -13,6 +13,6 @@ echo "NOW           <$NOW>"
 echo "NETTY_TMP     <$NETTY_TMP>"
 echo "SECURITY_OPT  <$SECURITY_OPT>"
 echo "JAVA_OPTS     <$JAVA_OPTS>"
-echo
+echo PORT
 
 java $SECURITY_OPT $NETTY_TMP $JAVA_OPTS -Dserver.port=$PORT -jar /workspace/app.jar
