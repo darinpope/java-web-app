c14286c9f887a553a3dba5cc67fd84fa0be5f8be
JorgeCantu247 <jorge.cantu@atomic32.com>
2023-06-07 12:02:22 -0600

    m

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ java -version
 NOW=$(date +"%Y-%m-%d %H:%M:%S %Z")
 NETTY_TMP=-Dio.netty.native.workdir=/tmp
 SECURITY_OPT=-Djava.security.egd=file:/dev/./urandom
-PORT=$(($1))  # Convertir a entero
+PORT=$(expr $APP_PORT + 0)  # Convertir a entero
 
 echo
 echo "TZ            <$TZ>"
